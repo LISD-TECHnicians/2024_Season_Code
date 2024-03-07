7b4c070e508b43d222d773034f71ed38255680d3
Hunter Rothman <rothmanhunterd@gmail.com>
2024-03-07 13:59:43 -0800

    ClimberSubsystem CANSparkMax to TalonFX

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -5,8 +5,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
 import frc.robot.Constants.ControllerConstants;
 
-import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
@@ -20,20 +20,32 @@ public class ClimberSubsystem extends SubsystemBase {
 
     climberLeft.setNeutralMode(NeutralModeValue.Brake);
     climberRight.setNeutralMode(NeutralModeValue.Brake);
+
+    climberRight.setControl(new Follower(climberLeft.getDeviceID(), true));
   }
   
-  
+  public void setClimberSpeed(double speed) {
+    if (speed > 0 && getUpperLimit()) {
+      climberLeft.setVoltage(0); 
+    }
+    else if (speed < 0 && getLowerLimit()) {
+      climberLeft.setVoltage(0);
+    }
+    else {
+      climberLeft.setVoltage(speed * ControllerConstants.NOMINAL_VOLTAGE);
+    }
+  }
 
-  public void setClimberSpeed(double speed) {}
-
-  public double getClimberPosition() {}
+  public double getClimberPosition() {
+    return climberLeft.getPosition().getValueAsDouble();
+  }
 
   public boolean getUpperLimit() {
-    return Math.abs(getClimberPosition() - climberLeft.getSoftLimit(SoftLimitDirection.kForward)) < ClimberConstants.CLIMBER_VARIABILITY;
+    return Math.abs(getClimberPosition() - ClimberConstants.CLIMBER_FORWARD_LIMIT) < ClimberConstants.CLIMBER_VARIABILITY;
   }
 
   public boolean getLowerLimit() {
-    return Math.abs(getClimberPosition() - climberLeft.getSoftLimit(SoftLimitDirection.kReverse)) < ClimberConstants.CLIMBER_VARIABILITY;
+    return Math.abs(getClimberPosition() - ClimberConstants.CLIMBER_REVERSE_LIMIT) < ClimberConstants.CLIMBER_VARIABILITY;
   }
 
   @Override
